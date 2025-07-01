d028de7557d048fb3b2ff3b79e412bef03c63a81
Rahul <RahulKumar703@outlook.com>
2025-07-01 16:39:16 +0530

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-background/95 backdrop-blur-[10px] bg-opacity-50">
       <Image
-        src={"/assets/images/logo.svg"}
+        src={"/assets/images/Logo.svg"}
         alt="Dr. Serena Blake, PsyD (Clinical Psychologist)"
         width={64}
         className="rounded-md animate-pulse"
